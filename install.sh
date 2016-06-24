4836136894d81e4e836d89115570bfcd1004aed7
H.Y.C <chenghsienyu@aim.com>
2016-06-24 22:10:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root"
+	exit
+fi
+
 cp -a startvideos.sh /home/pi/
 cp -a configfile.txt /boot/
-sudo chmod +x startvideos.sh
+chmod +x /home/pi/startvideos.sh
 apt-get remove omxplayer
 rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
 wget -O- http://yokto.net/0/omxplayer/omxplayer-3176db4.tar.bz2 | tar -C / -xjvf -
@@ -13,6 +19,9 @@ pip install pexpect
 mkdir /media/USB
 chmod a+r /media/USB
 echo "/dev/sda1	/media/USB	exfat	defaults,noatime 	0	0" >> /etc/fstab
-sed -i '/\fi/a setterm -blank 1' /etc/rc.local
-sed -i '/\fi/a sudo /home/pi/startvideos.sh &' /etc/rc.local
+sed -i -e '$i \setterm -blank 1\n' /etc/rc.local
+sed -i -e '$i \sudo /home/pi/startvideos.sh &\n' /etc/rc.local
+
+exit 0
+
 echo "enjoy!" 
