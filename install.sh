7fa36cbc4f6bf86eb47259c26b1afff8f0798626
H.Y.C <chenghsienyu@aim.com>
2016-06-24 20:31:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cp -a startvideos.sh /home/pi/
 cp -a configfile.txt /boot/
-chmod +x startvideos.sh
+sudo chmod +x startvideos.sh
 apt-get remove omxplayer
 rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
 wget -O- http://yokto.net/0/omxplayer/omxplayer-3176db4.tar.bz2 | tar -C / -xjvf -
@@ -14,5 +14,5 @@ mkdir /media/USB
 chmod a+r /media/USB
 echo "/dev/sda1	/media/USB	exfat	defaults,noatime 	0	0" >> /etc/fstab
 sed -i '/\fi/a setterm -blank 1' /etc/rc.local
-sed -i '/\fi/a sudo /home/pi/projects/startvideos.sh &' /etc/rc.local
+sed -i '/\fi/a sudo /home/pi/startvideos.sh &' /etc/rc.local
 echo "enjoy!" 
