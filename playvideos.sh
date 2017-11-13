ed7261034c58d01c315c45d63fea8932c2f7527f
H.Y.C <chenghsienyu@aim.com>
2017-11-13 17:38:31 +0800

    Update playvideos.sh

--- playvideos.sh
+++ playvideos.sh
@@ -42,7 +42,7 @@ else
                 current=0
         fi
 
-        /usr/bin/omxplayer-sync -"$role" -o "$audio_source" "$FILES${vids[$current]}"
+        /usr/bin/omxplayer-sync -"$role"u "$FILES${vids[$current]}"
 fi
 done
 
